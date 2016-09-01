6b8c1ac7cab22a4d5aef489b01d45a37776a5200
Juri.Minamiyama <j-minamiyama@froide-kk.co.jp>
2016-09-01 15:31:20 +0900

    ななこのセリフ更新

--- markov.py
+++ markov.py
@@ -24,7 +24,7 @@ class Markov:
                 w2 = keys[1]
                 break
             if count > len(self.table):
-                return "質問の内容を理解できませんでした"
+                return "ななこにも分かる言葉で言ってよぉ～☆"
 
         count = 0
         while w2 not in ["。", "！", "？", "!", "?", "."]:
